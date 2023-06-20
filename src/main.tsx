e4deede3b46ad9fe939f89e3c4651f5a2c4cc367
Theo <ekunnusitheophilus@gmail.com>
2023-06-21 00:16:53 +0100

    styles: global css changed to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './assets/css/global.css';
+import { GlobalStyles } from './assets/css/global.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
