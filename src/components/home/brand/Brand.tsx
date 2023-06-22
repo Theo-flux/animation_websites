efef22b03573f1f9065799089cec16dc13aa7ddc
Theo <ekunnusitheophilus@gmail.com>
2023-06-22 14:05:46 +0100

    style: brand ui component

--- src/components/home/brand/Brand.tsx
+++ src/components/home/brand/Brand.tsx
@@ -1,10 +1,53 @@
-import { BrandContainer } from './brand.css';
+import { BrandContainer, BrandGrid, BrandFigure, BrandImg } from './brand.css';
 import { Div } from '../../../shared';
+import Airbnb from '../../../assets/images/brands/airbnb.svg';
+import Google from '../../../assets/images/brands/google.svg';
+import Netflix from '../../../assets/images/brands/netflix.svg';
+import Slack from '../../../assets/images/brands/slack.svg';
+import Uber from '../../../assets/images/brands/uber.svg';
+
+const brands = [
+  {
+    name: 'slack',
+    image: Slack,
+  },
+
+  {
+    name: 'netflix',
+    image: Netflix,
+  },
+
+  {
+    name: 'google',
+    image: Google,
+  },
+
+  {
+    name: 'airbnb',
+    image: Airbnb,
+  },
+
+  {
+    name: 'uber',
+    image: Uber,
+  },
+];
 
 export default function Brand() {
   return (
     <BrandContainer>
-      <Div>Brand</Div>
+      <Div>
+        <BrandGrid>
+          {brands.map((brand, index) => {
+            const { name, image } = brand;
+            return (
+              <BrandFigure key={index}>
+                <BrandImg src={image} alt={name} />
+              </BrandFigure>
+            );
+          })}
+        </BrandGrid>
+      </Div>
     </BrandContainer>
   );
 }
--- src/components/home/brand/brand.css.ts
+++ src/components/home/brand/brand.css.ts
@@ -1,3 +1,51 @@
 import styled from 'styled-components';
+import { device } from '../../../utils';
 
-export const BrandContainer = styled.div``;
+export const BrandContainer = styled.div`
+  background-color: white;
+  padding-bottom: 2rem;
+`;
+
+export const BrandGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+
+
+  @media ${device.md} {
+    gap: 4rem;
+  }
+
+  @media ${device.lg} {
+    gap: 6rem;
+  }
+
+  @media ${device.xl} {
+    gap: 8rem;
+  }
+`;
+
+export const BrandFigure = styled.figure`
+  /* border: 1px solid magenta; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BrandImg = styled.img`
+  width: 50%;
+
+  @media ${device.md} {
+    width: 65%;
+  }
+
+  @media ${device.lg} {
+    width: 80%;
+  }
+
+  @media ${device.xl} {
+    width: 100%;
+  }
+`;
